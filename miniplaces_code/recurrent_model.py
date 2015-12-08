c279f615decb4f6a2208eb7c7990f389b3649dd5
Ben Schreck <bschreck@mit.edu>
2015-12-08 11:28:33 -0500

    recurrent model trains

--- miniplaces_code/recurrent_model.py
+++ miniplaces_code/recurrent_model.py
@@ -58,7 +58,7 @@ def _extract_glimpse_from_location(full_image, location):
     glimpse_size = tf.constant(FLAGS.glimpse_size, dtype=tf.int32, shape=[2])
     glimpse = tf.image.extract_glimpse(full_image,glimpse_size,
                             pixels_from_center, centered=True, normalized=False)
-    return glimpse
+    return tf.stop_gradient(glimpse)
 
 
 
@@ -244,6 +244,125 @@ def rnn_model(full_image):
     classifications = tf.pad(tf.pack(classifications_list), tf.convert_to_tensor([[0,FLAGS.max_recurrent_steps-step-1],[0,0],[0,0]]))
 
     classifications = attention(classifications, valid_classifications)
+    classifications.get_shape()
 
-    return classifications
+    return tf.squeeze(classifications)
+
+def loss(logits, labels):
+  """Add L2Loss to all the trainable variables.
+
+  Add summary for for "Loss" and "Loss/avg".
+  Args:
+    logits: Logits from inference().
+    labels: Labels from distorted_inputs or inputs(). 1-D tensor
+            of shape [batch_size]
+
+  Returns:
+    Loss tensor of type float.
+  """
+  # Reshape the labels into a dense Tensor of
+  # shape [batch_size, FLAGS.num_classes].
+  sparse_labels = tf.reshape(labels, [FLAGS.batch_size, 1])
+  indices = tf.reshape(tf.range(0, FLAGS.batch_size, 1), [FLAGS.batch_size, 1])
+  concated = tf.concat(1, [indices, sparse_labels])
+  dense_labels = tf.sparse_to_dense(concated,
+                                    [FLAGS.batch_size, FLAGS.num_classes],
+                                    1.0, 0.0)
+
+  # Calculate the average cross entropy loss across the batch.
+  print dense_labels.get_shape()
+  print logits.get_shape()
+  cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+      logits, dense_labels, name='cross_entropy_per_example')
+
+  cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+  tf.add_to_collection('losses', cross_entropy_mean)
+
+  # The total loss is defined as the cross entropy loss plus all of the weight
+  # decay terms (L2 loss).
+  return tf.add_n(tf.get_collection('losses'), name='total_loss')
+
+def _add_loss_summaries(total_loss):
+  """Add summaries for losses in CIFAR-10 model.
+
+  Generates moving average for all losses and associated summaries for
+  visualizing the performance of the network.
+
+  Args:
+    total_loss: Total loss from loss().
+  Returns:
+    loss_averages_op: op for generating moving averages of losses.
+  """
+  # Compute the moving average of all individual losses and the total loss.
+  loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+  losses = tf.get_collection('losses')
+  loss_averages_op = loss_averages.apply(losses + [total_loss])
+
+  # Attach a scalar summmary to all individual losses and the total loss; do the
+  # same for the averaged version of the losses.
+  for l in losses + [total_loss]:
+    # Name each loss as '(raw)' and name the moving average version of the loss
+    # as the original loss name.
+    tf.scalar_summary(l.op.name +' (raw)', l)
+    tf.scalar_summary(l.op.name, loss_averages.average(l))
+
+  return loss_averages_op
+
+
+def train(total_loss, global_step):
+  """Train CIFAR-10 model.
+
+  Create an optimizer and apply to all trainable variables. Add moving
+  average for all trainable variables.
+
+  Args:
+    total_loss: Total loss from loss().
+    global_step: Integer Variable counting the number of training steps
+      processed.
+  Returns:
+    train_op: op for training.
+  """
+  # Variables that affect learning rate.
+  num_batches_per_epoch = FLAGS.num_examples_per_epoch_for_train / FLAGS.batch_size
+  decay_steps = int(num_batches_per_epoch * FLAGS.num_epochs_per_decay)
+
+  # Decay the learning rate exponentially based on the number of steps.
+  lr = tf.train.exponential_decay(FLAGS.initial_learning_rate,
+                                  global_step,
+                                  decay_steps,
+                                  FLAGS.learning_rate_decay_factor,
+                                  staircase=True)
+  tf.scalar_summary('learning_rate', lr)
+
+  # Generate moving averages of all losses and associated summaries.
+  loss_averages_op = _add_loss_summaries(total_loss)
+
+  # Compute gradients.
+  with tf.control_dependencies([loss_averages_op]):
+    opt = tf.train.GradientDescentOptimizer(lr)
+    grads = opt.compute_gradients(total_loss)
+
+  # Apply gradients.
+  apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+  # Add histograms for trainable variables.
+  for var in tf.trainable_variables():
+    tf.histogram_summary(var.op.name, var)
+
+  # Add histograms for gradients.
+  for grad, var in grads:
+    if grad:
+      tf.histogram_summary(var.op.name + '/gradients', grad)
+
+  # Track the moving averages of all trainable variables.
+  variable_averages = tf.train.ExponentialMovingAverage(
+      FLAGS.moving_average_decay, global_step)
+  variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+  with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
+    train_op = tf.no_op(name='train')
+
+  return train_op
+
+
 
--- miniplaces_code/rnn_train.py
+++ miniplaces_code/rnn_train.py
@@ -10,11 +10,11 @@ FLAGS = tf.app.flags.FLAGS
 # Basic model parameters.
 tf.app.flags.DEFINE_integer('batch_size', 16,
                             """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_string('data_dir', '/local/miniplaces/images',
+tf.app.flags.DEFINE_string('data_dir', '/Users/bschreck/scenic-recursion/images',
                            """Path to the miniplaces data directory.""")
-tf.app.flags.DEFINE_string('label_dir', '/local/miniplaces/development_kit/data',
+tf.app.flags.DEFINE_string('label_dir', '/Users/bschreck/scenic-recursion/development_kit/data',
                            """Path to the miniplaces label directory.""")
-tf.app.flags.DEFINE_string('train_dir', '/local/miniplaces/train_output',
+tf.app.flags.DEFINE_string('train_dir', '/Users/bschreck/senic-recursion/train_output',
                            """Path to the miniplaces data directory.""")
 tf.app.flags.DEFINE_integer('image_size', 100,"""width of image to crop to for training""")
 tf.app.flags.DEFINE_integer('glimpse_size', 32,"""width of image to extract glimpse for rnn step""")
@@ -57,6 +57,20 @@ def train():
         # Build a Graph that computes the logits predictions from the
         # inference model.
         logits = model.rnn_model(images)
+        # Calculate loss.
+        loss = model.loss(logits, labels)
+
+        n = tf.zeros([1], dtype=tf.int32)
+
+        # Build a Graph that trains the model with one batch of examples and
+        # updates the model parameters.
+        train_op = model.train(loss, global_step)
+
+        # Create a saver.
+        saver = tf.train.Saver(tf.all_variables())
+
+        # Build the summary operation based on the TF collection of Summaries.
+        summary_op = tf.merge_all_summaries()
 
         # # Build an initialization operation to run below.
         init = tf.initialize_all_variables()
@@ -71,18 +85,46 @@ def train():
         threads = tf.train.start_queue_runners(coord=coord,sess=sess)
         sess.run(label_enqueue)
 
-        for step in xrange(32):
+        summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
+                                                graph_def=sess.graph_def)
+
+
+        for step in xrange(FLAGS.max_steps):
+            print 'step:',step
             start_time = time.time()
-            print "running:"
-            logit_outputs = sess.run(logits)
-
-            n = tf.zeros([1], dtype=tf.int32)
-            logit_outputs = tf.Print(logit_outputs, [logit_outputs], message='outputs:')
-            print "ran:"
-            print logit_outputs.get_shape()
+            _, loss_value = sess.run([train_op, loss])
+            print 'ran'
             duration = time.time() - start_time
-        print duration
 
+            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            if step % 10 == 0:
+                num_examples_per_step = FLAGS.batch_size
+                examples_per_sec = num_examples_per_step / float(duration)
+                sec_per_batch = float(duration)
+
+                format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                      'sec/batch)')
+                print format_str % (datetime.now(), step, loss_value,
+                             examples_per_sec, sec_per_batch)
+            print 'here'
+            if step % 100 == 0:
+                print "entering summary"
+                summary_str = sess.run(summary_op)
+                summary_writer.add_summary(summary_str, step)
+                print "exiting summary"
+
+            # Save the model checkpoint periodically.
+            if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+                print "ckpt"
+                checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
+                print "leaving ckpt"
+
+            #TODO: check to make sure this is right
+            if step > 0 and step % (FLAGS.num_examples_per_epoch_for_train -1) == 0:
+                sess.run(label_enqueue)
+            print "about to start second"
         coord.request_stop()
         coord.join(threads)
 
